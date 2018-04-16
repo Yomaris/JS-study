95c53cb543a9971bf96feb99928367b699465858
Yomaris <caotlyx@icloud.com>
2018-04-17 00:14:48 +0800

    music

---
+++ music-player/index.js
@@ -0,0 +1,18 @@
+//分析Node.js 的 HTTP 服务器：
+//第一行请求（require）Node.js 自带的 http 模块，并且把它赋值给 http 变量。
+//接下来我们调用 http 模块提供的函数： createServer 。这个函数会返回 一个对象，这个对象有一个叫做 listen 的方法，这个方法有一个数值参数， 指定这个 HTTP 服务器监听的端口号。
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
+var path = require('path');
+
+//create server
+http.createServer(function (request, response) {
+    //Parse the request,include the file name
+    var pathObj = url.parse(request.url);
+
+    response.end(fs.readFileSync(__dirname + pathObj.pathname));
+
+}).listen(8080);
+// 控制台会输出以下信息
+console.log('Server running at http://127.0.0.1:8080/');
